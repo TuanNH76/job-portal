588d31577a7d5e2264b0e0444482faf92fb2bf74
Marcus Truong <truong22082002@gmail.com>
2024-05-09 10:53:34 +0700

    add homepage layout #34

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import HomeRouter from "./router/HomeRouter";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="font-roboto">
+      <Router>
+        <Routes>
+          <Route path="/*" element={<HomeRouter />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
