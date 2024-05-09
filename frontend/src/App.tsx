71a599f3e823a624691bb5f3947750e98910f7ed
Marcus Truong <truong22082002@gmail.com>
2024-05-09 09:44:00 +0700

    start merging fe with be #33

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,9 @@
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import UserRouter from "./routes/user/UserRouter";
-import EmployerRouter from "./routes/employer/EmployerRouter";
-import NotFound from "./components/Error/NotFound";
-import HomeRouter from "./routes/home/HomeRouter";
-import JobRouter from "./routes/jobs/JobRouter";
-import ApplicationRouter from "./routes/applications/applicationRouter";
-import MessengerRouter from "./routes/messenger/MessengerRouter";
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="font-roboto">
-      <Router>
-        <Routes>
-          <Route path="/*" element={<HomeRouter />} />
-          <Route path="/user/*" element={<UserRouter />} />
-          <Route path="/employer/*" element={<EmployerRouter />} />
-          <Route path="/job/*" element={<JobRouter />} />
-          <Route path="/application/*" element={<ApplicationRouter />} />
-          <Route path="/messenger/*" element={<MessengerRouter />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
