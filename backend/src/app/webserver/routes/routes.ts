8069f2379357cbe6bef18665f50c2c4c9c60faff
juny76 <tuannguyenhuy87@gmail.com>
2024-05-13 23:38:40 +0700

    add route employer in routes #31

--- backend/src/app/webserver/routes/routes.ts
+++ backend/src/app/webserver/routes/routes.ts
@@ -4,11 +4,13 @@ import jobRouter from "./jobs";
 import { Application } from "express";
 import userRouter from "./user";
 import authenticationMiddleware from "../middleware/authenticationMiddleware";
+import employerRouter from "./employer";
 
 
 const routes = (app: Application) => {
     app.use('/api/user', userRouter());
     app.use('/api/user-auth', userAuthRouter());
+    app.use('/api/employer',employerRouter());
     app.use('/api/employer-auth', employerAuthRouter());
     app.use('/api/job', authenticationMiddleware, jobRouter());
 
