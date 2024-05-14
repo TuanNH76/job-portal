04b5eb70cda044261b50183fd499d021872e5d0f
Marcus Truong <truong22082002@gmail.com>
2024-05-14 08:22:13 +0700

    #41

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import HomeRouter from "./router/HomeRouter";
 import UserRouter from "./router/UserRouter";
 
+
 const App = () => {
   return (
     <div className="font-roboto">
--- frontend/src/router/UserRouter.tsx
+++ frontend/src/router/UserRouter.tsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import UserLoginPage from "../pages/user/UserLoginPage";
 import UserSignupPage from "../pages/user/UserSignupPage";
+import UserHomePage from "../pages/user/UserHomePage";
+import UserProfilePage from "../pages/user/UserProfilepage";
+import EditUserProfilePage from "../pages/user/EditUserProfilePage";
+import UserJobApplicationsPage from "../pages/user/UserJobApplicationsPage";
 
 const UserRouter = () => {
   return (
     <div>
       <Routes>
+        <Route path="/home" element={<UserHomePage />} />
         <Route path="/login" element={<UserLoginPage />} />
         <Route path="/register" element={<UserSignupPage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
+        <Route path="/edit-profile" element={<EditUserProfilePage />} />
+        <Route path="/all-applications" element={<UserJobApplicationsPage />} />
       </Routes>
     </div>
   );
