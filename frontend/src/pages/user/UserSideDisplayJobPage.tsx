5e6f92c8637a64ebda2d9500a033d3d450ea725b
Marcus Truong <truong22082002@gmail.com>
2024-05-16 20:50:32 +0700

    add view job user, apply #51

---
+++ frontend/src/pages/user/UserSideDisplayJobPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import UserHeader from "../../components/Header/UserHeader";
+import UserSideFooter from "../../components/Footer/UserSideFooter";
+import DisplayJobs from "../../components/User/Jobs/DisplayJobs";
+
+function DisplayJobPage() {
+  return (
+    <>
+      <UserHeader />
+      <DisplayJobs />
+      <UserSideFooter />
+    </>
+  );
+}
+
+export default DisplayJobPage;
--- frontend/src/router/JobRouter.tsx
+++ frontend/src/router/JobRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import AddNewJob from "../pages/employer/AddNewJob";
 import EditJobPage from "../pages/employer/EditJobPage";
 import ViewJobPage from "../pages/employer/ViewJobPage";
+import DisplayJobPage from "../pages/user/UserSideDisplayJobPage";
 
 const JobRouter = () => {
   return (
@@ -9,6 +10,7 @@ const JobRouter = () => {
       <Routes>
         <Route path="/create-job" element={<AddNewJob />} />
         <Route path="/edit-job/:id" element={<EditJobPage />} />
+        <Route path="/all-jobs" element={<DisplayJobPage />} />
         <Route path="/view-job" element={<ViewJobPage />} />
       </Routes>
     </div>
